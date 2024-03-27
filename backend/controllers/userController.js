5e21caebcb8597bc3de4881aee94d563bbe9d864
Parth18 <48610764+Mazrah18@users.noreply.github.com>
2024-03-26 18:59:08 -0700

    debugging

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,6 +44,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
+  console.log(process.env.SECRET_KEY)
   return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
 }
 
